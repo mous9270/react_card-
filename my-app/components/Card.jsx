fa85eac9f6a249158339de0ea15ffe58df922cf8
mous9270 <mous9270222@gmail.com>
2024-07-23 09:08:08 -0400

    Basic card made. The image bug is fixed using nextjs Image tag.

--- my-app/components/Card.jsx
+++ my-app/components/Card.jsx
@@ -1,9 +1,10 @@
-import devpic from "../public/devchallenges.png"
+import devpic from '../public/devchallenges.png';
+import Image from 'next/image';
 
 function Card()  {
     return(
          <div className="card">
-            <img classNamesrc={devpic} alt="image" />
+            <Image src={devpic} alt="image" />
             <h2>Srinivas</h2>
             <p>I am a Programmer.</p>
          </div>
