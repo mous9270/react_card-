354c8e209e3030bafc86c2b1f489a305165bcc60
mous9270 <mous9270222@gmail.com>
2024-07-23 07:13:17 -0400

    create card component The card component is created but he image is not visible.

---
+++ my-app/app/page.js
@@ -0,0 +1,8 @@
+import styles from "./page.module.css";
+import Card from "../components/Card.jsx";
+
+export default function Home() {
+  return (
+    <Card />
+  );    
+}
---
+++ my-app/components/Card.jsx
@@ -0,0 +1,15 @@
+import devpic from "../public/devchallenges.png"
+
+function Card()  {
+    return(
+         <div className="card">
+            <img classNamesrc={devpic} alt="image" />
+            <h2>Srinivas</h2>
+            <p>I am a Programmer.</p>
+         </div>
+
+       
+    );
+}
+
+export default Card
